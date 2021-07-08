0760f7984df3763eee71a03d9637241eb56b07d5
alex <alexizana10@gmail.com>
2021-07-08 18:22:42 +0400

    added playCircle icon to single card

--- src/components/Cards/SingleCard.js
+++ src/components/Cards/SingleCard.js
@@ -1,11 +1,26 @@
 import React from "react";
 
-import classes from "./SignleCard.module.css";
 import Popup from "../Popup/Popup";
+import playCircleIcon from "../../assets/playCirlce.svg";
+
+import classes from "./SignleCard.module.css";
 
 const SingleCard = ({ imgSrc, altText }) => {
   return (
-    <div className={classes["single-card"]}>
+    <div
+      className={classes["single-card"]}
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img
+        src={playCircleIcon}
+        alt="playCircle"
+        style={{ position: "absolute" }}
+        className={classes.onHover}
+      />
       <img src={imgSrc} alt={altText} className={classes["card-image"]} />
       <Popup onHover={classes.onHover} />
     </div>
