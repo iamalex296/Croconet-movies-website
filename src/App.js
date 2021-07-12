2414038cb35553dca37fcefaad986577319671ce
alex <alexizana10@gmail.com>
2021-07-12 14:17:33 +0400

    added routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
+import React from "react";
+
+import { Route, BrowserRouter as Router, Redirect } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import MoviesPage from "./pages/MoviesPage";
+import SeriesPage from "./pages/SeriesPage";
+
 import Header from "./components/Header/Header";
 
 import "./App.css";
-import ContentLayout from "./components/Layout/ContentLayout";
+
 import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <ContentLayout />
-      <Footer />
+      <Router>
+        <Header />
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home" exact>
+          <HomePage />
+        </Route>
+        <Route path="/movies">
+          <MoviesPage />
+        </Route>
+        <Route path="/series">
+          <SeriesPage />
+        </Route>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/pages/SeriesPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SeriesPage = () => {
+  return <div>Series Page</div>;
+};
+
+export default SeriesPage;
