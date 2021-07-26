84ed17c7a8a50d3c8c90b907ef301b457014073c
alex <alexizana10@gmail.com>
2021-07-26 19:42:40 +0400

    changed files structures

--- src/components/Layout/SignleMoviePageLayout.js
+++ src/components/Layout/SignleMoviePageLayout.js
@@ -1,18 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+
+import { useParams } from "react-router-dom";
 
 import Template from "../Templates/Template";
 // import classes from "./SingleMoviePageLayout.module.css";
 
 const poster_URL = "https://image.tmdb.org/t/p/original";
 
-const SignleMoviePageLayout = ({
-  singleMovieDetailsObj,
-  movieVideo,
-  isLoading,
-}) => {
-  // console.log("singleMovieDetails", singleMovieDetailsObj);
-  // console.log("video", movieVideo);
-  // console.log("isLoading", isLoading);
+const SignleMoviePageLayout = ({ setError, API_key }) => {
+  const params = useParams();
+  // console.log("params", params);
+  const [singleMovieDetailsObj, setSingleMovieDetailObj] = useState("");
+  const [movieVideo, setMovieVideo] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const singleMovie_URL = `https://api.themoviedb.org/3/movie/${params.movieId}?api_key=${API_key}&language=en-US`;
+  const movieVideo_URL = `https://api.themoviedb.org/3/movie/${singleMovieDetailsObj.id}/videos?api_key=${API_key}&language=en-US`;
+
+  useEffect(() => {
+    const getSingleMovieDetailsData = async () => {
+      await axios
+        .get(singleMovie_URL)
+        .then((response) => {
+          // console.log("singleMovieDetailsData", response.data);
+          setSingleMovieDetailObj(response.data);
+        })
+        .catch((error) => setError(error));
+    };
+    getSingleMovieDetailsData();
+  }, [singleMovie_URL, setError]);
+
+  useEffect(() => {
+    if (singleMovieDetailsObj) {
+      const getSingleMovieVideo = async () => {
+        await axios
+          .get(movieVideo_URL)
+          .then((response) => {
+            // console.log("movieVideoData", response.data);
+            if (response.data) {
+              if (response.data.results[0]) {
+                let video = [];
+                const singleVideo = response.data.results[0];
+                video.push(
+                  <iframe
+                    style={{ border: "none" }}
+                    type="text/html"
+                    width="100%"
+                    height="500px"
+                    src={`https://www.youtube.com/embed/${singleVideo.key}`}
+                    title={singleVideo.name}
+                    key={singleVideo.key}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  ></iframe>
+                );
+                setIsLoading(false);
+                setMovieVideo(video);
+              } else {
+                setIsLoading(false);
+              }
+            }
+          })
+          .catch((error) => setError(error));
+      };
+      getSingleMovieVideo();
+    }
+  }, [movieVideo_URL, setError, singleMovieDetailsObj]);
 
   return (
     <Template
@@ -56,7 +110,23 @@ const SignleMoviePageLayout = ({
                 padding: "0",
               }}
             >
-              {movieVideo}
+              {movieVideo ? (
+                movieVideo
+              ) : (
+                <p
+                  style={{
+                    textAlign: "center",
+                    fontSize: "36px",
+                    backgroundColor: "#0083CA",
+                    padding: "50px",
+                    borderRadius: "5px",
+                    opacity: "0.9",
+                    margin: "150px auto",
+                  }}
+                >
+                  ფილმი მალე დაემატება...
+                </p>
+              )}
             </div>
           </div>
 
--- src/pages/SignleMoviePage.js
+++ src/pages/SignleMoviePage.js
@@ -1,95 +1,9 @@
-import React, { useState, useEffect } from "react";
-
-import axios from "axios";
-
-import { useParams } from "react-router-dom";
+import React from "react";
 
 import SingleMoviePageLayout from "../components/Layout/SignleMoviePageLayout";
 
 const SignleMoviePage = ({ API_key, setError }) => {
-  const params = useParams();
-  // console.log("params", params);
-  const [singleMovieDetailsObj, setSingleMovieDetailObj] = useState("");
-  const [movieVideo, setMovieVideo] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
-
-  const singleMovie_URL = `https://api.themoviedb.org/3/movie/${params.movieId}?api_key=${API_key}&language=en-US`;
-  const movieVideo_URL = `https://api.themoviedb.org/3/movie/${singleMovieDetailsObj.id}/videos?api_key=${API_key}&language=en-US`;
-
-  useEffect(() => {
-    const getSingleMovieDetailsData = async () => {
-      await axios
-        .get(singleMovie_URL)
-        .then((response) => {
-          // console.log("singleMovieDetailsData", response.data);
-          setSingleMovieDetailObj(response.data);
-        })
-        .catch((error) => setError(error));
-    };
-    getSingleMovieDetailsData();
-  }, [singleMovie_URL, setError]);
-
-  useEffect(() => {
-    if (singleMovieDetailsObj) {
-      const getSingleMovieVideo = async () => {
-        await axios
-          .get(movieVideo_URL)
-          .then((response) => {
-            // console.log("movieVideoData", response.data);
-            if (response.data) {
-              if (response.data.results[0]) {
-                let video = [];
-                const singleVideo = response.data.results[0];
-                video.push(
-                  <iframe
-                    style={{ border: "none" }}
-                    type="text/html"
-                    width="100%"
-                    height="500px"
-                    src={`https://www.youtube.com/embed/${singleVideo.key}`}
-                    title={singleVideo.name}
-                    key={singleVideo.key}
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  ></iframe>
-                );
-                setIsLoading(false);
-                setMovieVideo(video);
-              } else {
-                setIsLoading(false);
-              }
-            }
-          })
-          .catch((error) => setError(error));
-      };
-      getSingleMovieVideo();
-    }
-  }, [movieVideo_URL, setError, singleMovieDetailsObj]);
-
-  return (
-    <SingleMoviePageLayout
-      singleMovieDetailsObj={singleMovieDetailsObj}
-      movieVideo={
-        movieVideo ? (
-          movieVideo
-        ) : (
-          <p
-            style={{
-              textAlign: "center",
-              fontSize: "36px",
-              backgroundColor: "#0083CA",
-              padding: "50px",
-              borderRadius: "5px",
-              opacity: "0.9",
-              margin: "150px auto",
-            }}
-          >
-            ფილმი მალე დაემატება...
-          </p>
-        )
-      }
-      isLoading={isLoading}
-    />
-  );
+  return <SingleMoviePageLayout setError={setError} API_key={API_key} />;
 };
 
 export default SignleMoviePage;
